47bea554a7859b76f90945f5575e13f13ab36b85
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2014-12-16 15:16:59 +0100

    JavammXAssignmentImplCustom
    Change-Id: I6f35b5a6b7f7a71fb92902ec134510d800fa42a6

---
+++ javamm/src/javamm/javamm/impl/JavammXAssignmentImplCustom.java
@@ -0,0 +1,173 @@
+/**
+ * 
+ */
+package javamm.javamm.impl;
+
+import javamm.javamm.JavammPackage;
+import javamm.javamm.JavammXAssignment;
+
+import org.eclipse.emf.common.notify.Notification;
+import org.eclipse.emf.common.notify.NotificationChain;
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.InternalEObject;
+import org.eclipse.emf.ecore.impl.ENotificationImpl;
+import org.eclipse.xtext.xbase.XExpression;
+import org.eclipse.xtext.xbase.impl.XAssignmentImplCustom;
+
+/**
+ * @author Lorenzo Bettini
+ *
+ */
+public class JavammXAssignmentImplCustom extends XAssignmentImplCustom implements JavammXAssignment {
+
+	/**
+	 * The cached value of the '{@link #getIndex() <em>Index</em>}' containment reference.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #getIndex()
+	 * @generated
+	 * @ordered
+	 */
+	protected XExpression index;
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected EClass eStaticClass()
+	{
+		return JavammPackage.Literals.JAVAMM_XASSIGNMENT;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public XExpression getIndex()
+	{
+		return index;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public NotificationChain basicSetIndex(XExpression newIndex, NotificationChain msgs)
+	{
+		XExpression oldIndex = index;
+		index = newIndex;
+		if (eNotificationRequired())
+		{
+			ENotificationImpl notification = new ENotificationImpl(this, Notification.SET, JavammPackage.JAVAMM_XASSIGNMENT__INDEX, oldIndex, newIndex);
+			if (msgs == null) msgs = notification; else msgs.add(notification);
+		}
+		return msgs;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public void setIndex(XExpression newIndex)
+	{
+		if (newIndex != index)
+		{
+			NotificationChain msgs = null;
+			if (index != null)
+				msgs = ((InternalEObject)index).eInverseRemove(this, EOPPOSITE_FEATURE_BASE - JavammPackage.JAVAMM_XASSIGNMENT__INDEX, null, msgs);
+			if (newIndex != null)
+				msgs = ((InternalEObject)newIndex).eInverseAdd(this, EOPPOSITE_FEATURE_BASE - JavammPackage.JAVAMM_XASSIGNMENT__INDEX, null, msgs);
+			msgs = basicSetIndex(newIndex, msgs);
+			if (msgs != null) msgs.dispatch();
+		}
+		else if (eNotificationRequired())
+			eNotify(new ENotificationImpl(this, Notification.SET, JavammPackage.JAVAMM_XASSIGNMENT__INDEX, newIndex, newIndex));
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public NotificationChain eInverseRemove(InternalEObject otherEnd, int featureID, NotificationChain msgs)
+	{
+		switch (featureID)
+		{
+			case JavammPackage.JAVAMM_XASSIGNMENT__INDEX:
+				return basicSetIndex(null, msgs);
+		}
+		return super.eInverseRemove(otherEnd, featureID, msgs);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public Object eGet(int featureID, boolean resolve, boolean coreType)
+	{
+		switch (featureID)
+		{
+			case JavammPackage.JAVAMM_XASSIGNMENT__INDEX:
+				return getIndex();
+		}
+		return super.eGet(featureID, resolve, coreType);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public void eSet(int featureID, Object newValue)
+	{
+		switch (featureID)
+		{
+			case JavammPackage.JAVAMM_XASSIGNMENT__INDEX:
+				setIndex((XExpression)newValue);
+				return;
+		}
+		super.eSet(featureID, newValue);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public void eUnset(int featureID)
+	{
+		switch (featureID)
+		{
+			case JavammPackage.JAVAMM_XASSIGNMENT__INDEX:
+				setIndex((XExpression)null);
+				return;
+		}
+		super.eUnset(featureID);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public boolean eIsSet(int featureID)
+	{
+		switch (featureID)
+		{
+			case JavammPackage.JAVAMM_XASSIGNMENT__INDEX:
+				return index != null;
+		}
+		return super.eIsSet(featureID);
+	}
+}
