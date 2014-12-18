bff40b6f0815ec0023a76c8747032709b5d0fea0
Lorenzo Bettini <bettini@dsi.unifi.it>
2014-12-18 19:13:57 +0100

    removed getDeclaredType
    Change-Id: Ibcd4536ef752f7cd59781ab8660f4cb3acb0f169

--- javamm/src/javamm/typesystem/JavammExpressionArgumentFactory.java
+++ javamm/src/javamm/typesystem/JavammExpressionArgumentFactory.java
@@ -5,13 +5,7 @@ package javamm.typesystem;
 
 import javamm.javamm.JavammXAssignment;
 
-import org.eclipse.xtext.common.types.JvmField;
-import org.eclipse.xtext.common.types.JvmFormalParameter;
-import org.eclipse.xtext.common.types.JvmIdentifiableElement;
-import org.eclipse.xtext.common.types.JvmOperation;
-import org.eclipse.xtext.common.types.JvmTypeReference;
 import org.eclipse.xtext.xbase.XExpression;
-import org.eclipse.xtext.xbase.XVariableDeclaration;
 import org.eclipse.xtext.xbase.typesystem.arguments.AssignmentFeatureCallArguments;
 import org.eclipse.xtext.xbase.typesystem.arguments.IFeatureCallArguments;
 import org.eclipse.xtext.xbase.typesystem.internal.AbstractLinkingCandidate;
@@ -42,21 +36,5 @@ public class JavammExpressionArgumentFactory extends ExpressionArgumentFactory {
 		
 		return super.createExpressionArguments(expression, candidate);
 	}
-	
-	protected JvmTypeReference getDeclaredType(JvmIdentifiableElement identifiable) {
-		if (identifiable instanceof XVariableDeclaration) {
-			return ((XVariableDeclaration)identifiable).getType();
-		}
-		if (identifiable instanceof JvmOperation) {
-			return ((JvmOperation) identifiable).getReturnType();
-		}
-		if (identifiable instanceof JvmField) {
-			return ((JvmField) identifiable).getType();
-		}
-		if (identifiable instanceof JvmFormalParameter) {
-			JvmTypeReference parameterType = ((JvmFormalParameter) identifiable).getParameterType();
-			return parameterType;
-		}
-		return null;
-	}
+
 }
