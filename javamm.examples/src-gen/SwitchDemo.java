c64914368f5042a22f256cb6e523d60d5c3bba0f
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2015-01-07 16:50:39 +0100

    added SwitchDemo in examples
    Change-Id: If1724f2404b20e0343376a916c5b0ced904fd167

---
+++ javamm.examples/src-gen/SwitchDemo.java
@@ -0,0 +1,58 @@
+@SuppressWarnings("all")
+public class SwitchDemo {
+  public static void printMonthString(int month) {
+    String monthString = null;
+    switch (month) {
+      case 0:
+      case 1:
+        monthString = "January";
+        break;
+      case 2:
+        monthString = "February";
+        break;
+      case 3:
+        monthString = "March";
+        break;
+      case 4:
+        monthString = "April";
+        break;
+      case 5:
+        monthString = "May";
+        break;
+      case 6:
+        monthString = "June";
+        break;
+      case 7:
+        monthString = "July";
+        break;
+      case 8:
+        monthString = "August";
+        break;
+      case 9:
+        monthString = "September";
+        break;
+      case 10:
+        monthString = "October";
+        break;
+      case 11:
+        monthString = "November";
+        break;
+      case 12:
+        monthString = "December";
+        break;
+      default:
+        {
+          monthString = "Invalid month";
+          break;
+        }
+    }
+    System.out.println(monthString);
+  }
+  
+  public static void main(String[] args) {
+    SwitchDemo.printMonthString(1);
+    SwitchDemo.printMonthString(8);
+    SwitchDemo.printMonthString(12);
+    SwitchDemo.printMonthString(15);
+  }
+}
