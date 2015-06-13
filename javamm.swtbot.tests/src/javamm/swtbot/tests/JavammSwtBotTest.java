2ac162c0360acb5897cc861afc4adb84162c67c4
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2015-06-13 18:29:39 +0200

    6: Updated SWTBot test for Run As/Debug As Java-- Application
    Change-Id: Iaae1a883ad1d8af569ad6ab2de2629a3fb836cc2
    Task-Url: http://github.com/LorenzoBettini/javamm/issues/issue/6

--- javamm.swtbot.tests/src/javamm/swtbot/tests/JavammSwtBotTest.java
+++ javamm.swtbot.tests/src/javamm/swtbot/tests/JavammSwtBotTest.java
@@ -18,6 +18,8 @@ import org.junit.runner.RunWith;
 @RunWith(SWTBotJunit4ClassRunner.class)
 public class JavammSwtBotTest extends AbstractJavammSwtbotTest {
 	
+	private static final String JAVAMM_APPLICATION = "2 Java-- Application";
+
 	@Test
 	public void canCreateANewJavammProject() throws CoreException {
 		createProjectAndAssertNoErrorMarker(PROJECT_TYPE);
@@ -45,8 +47,10 @@ public class JavammSwtBotTest extends AbstractJavammSwtbotTest {
 				expand().expandNode("src").
 				expandNode("javamm").
 				getNode("HelloWorld.javamm");
-		tree.contextMenu("Run As").menu("1 Java Application");
-		tree.contextMenu("Debug As").menu("1 Java Application");
+		// apparently, on a new workbench, any file has "Run As Java Application" as the
+		// first menu, so we need to look for the second entry
+		tree.contextMenu("Run As").menu(JAVAMM_APPLICATION);
+		tree.contextMenu("Debug As").menu(JAVAMM_APPLICATION);
 	}
 
 }
