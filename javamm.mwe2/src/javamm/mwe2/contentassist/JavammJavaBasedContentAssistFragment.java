23313522cfe5437304ae6c5fb2c4f860a7f1f2ad
Lorenzo Bettini <bettini@dsi.unifi.it>
2015-06-30 12:22:33 +0200

    custom Java based content assist fragment
    Change-Id: Idb834e7bb4013522941e2c244fe4b1bd43591044

---
+++ javamm.mwe2/src/javamm/mwe2/contentassist/JavammJavaBasedContentAssistFragment.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package javamm.mwe2.contentassist;
+
+import org.eclipse.xtext.ui.generator.contentAssist.JavaBasedContentAssistFragment;
+
+/**
+ * Just to have the same name of the Xpand template.
+ * 
+ * @author Lorenzo Bettini
+ *
+ */
+public class JavammJavaBasedContentAssistFragment extends JavaBasedContentAssistFragment {
+
+}
