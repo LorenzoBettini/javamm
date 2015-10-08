5d3f2fe7e3a15daa1039688a217633b8c4b7cb02
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2015-10-08 12:20:55 +0200

    27: added Input utility class
    Change-Id: I9aabf48f94135f74cc787095bd7fc82a33505fad
    Task-Url: http://github.com/LorenzoBettini/javamm/issues/issue/27

---
+++ javamm.runtime/src/javamm/util/Input.java
@@ -0,0 +1,77 @@
+package javamm.util;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+/**
+ * @author Pierluigi Crescenzi - Initial contribution and API
+ *
+ */
+public class Input {
+	public static int getInt(String message) {
+		InputFrame inf = new InputFrame(message);
+		String text = inf.text;
+		int number = 0;
+		boolean done = false;
+		while (!done) {
+			try {
+				number = Integer.parseInt(text);
+				done = true;
+			} catch (NumberFormatException nfe) {
+				System.out.println("HERE");
+				continue;
+			}
+		}
+		return (number);
+	}
+
+	public static long getLong(String message) {
+		InputFrame inf = new InputFrame(message);
+		String text = inf.text;
+		long number;
+		try {
+			number = Long.parseLong(text);
+		} catch (NumberFormatException nfe) {
+			number = 0;
+		}
+		return (number);
+	}
+
+	public static double getDouble(String message) {
+		InputFrame inf = new InputFrame(message);
+		String text = inf.text;
+		double number;
+		try {
+			number = Double.parseDouble(text);
+		} catch (NumberFormatException nfe) {
+			number = 0;
+		}
+		return (number);
+	}
+
+	public static char getChar(String message) {
+		InputFrame inf = new InputFrame(message);
+		if (inf.text.length() > 0) {
+			return (inf.text.charAt(0));
+		} else {
+			return (' ');
+		}
+	}
+
+	public static String getString(String message) {
+		InputFrame inf = new InputFrame(message);
+		return (inf.text);
+	}
+}
+
+class InputFrame extends JFrame {
+	private static final long serialVersionUID = 1L;
+
+	String text;
+
+	public InputFrame(String message) {
+		super();
+		text = JOptionPane.showInputDialog(this, message);
+		dispose();
+	}
+}
