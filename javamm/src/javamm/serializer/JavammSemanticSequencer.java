fd1026b6ab7800687d19db85a9d41471c020bee3
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2015-11-17 14:50:19 +0100

    45: JavammFeatureCallWithArrayAccess in the grammar
    Change-Id: Ie763f4cd6d25f6e5ece807056cfff42c6f922278
    Task-Url: https://github.com/LorenzoBettini/javamm/issues/issue/45

--- javamm/src/javamm/serializer/JavammSemanticSequencer.java
+++ javamm/src/javamm/serializer/JavammSemanticSequencer.java
@@ -25,40 +25,40 @@ import javamm.services.JavammGrammarAccess.XMemberFeatureCallElements;
  */
 public class JavammSemanticSequencer extends XbaseSemanticSequencerAccess {
 
-	@Inject
-	private JavammGrammarAccess access;
-
-	@Override
-	protected void sequence_XMemberFeatureCall(EObject context, JavammXMemberFeatureCall featureCall) {
-		INodesForEObjectProvider nodes = createNodeProvider(featureCall);
-		SequenceFeeder acceptor = createSequencerFeeder(featureCall, nodes);
-		XMemberFeatureCallElements memberFeatureCallElements= access.getXMemberFeatureCallAccess();
-
-		// we manually modify the structure of this element, so we must serialize that accordingly
-		acceptor.accept(memberFeatureCallElements.getJavammXMemberFeatureCallMemberCallTargetAction_1_1_0_0_0(),
-				featureCall.getArrayAccessExpression().getArray());
-
-		final List<XExpression> indexes = featureCall.getIndexes();
-		acceptor.accept(memberFeatureCallElements.getIndexesXExpressionParserRuleCall_1_1_0_0_2_0(),
-				indexes.get(0), 0);
-		for (int i = 1; i < indexes.size(); i++) {
-			acceptor.accept(memberFeatureCallElements.getIndexesXExpressionParserRuleCall_1_1_0_0_4_1_0(),
-					indexes.get(i), i);
-		}
-
-		acceptor.accept(memberFeatureCallElements.getFeatureJvmIdentifiableElementIdOrSuperParserRuleCall_1_1_1_0_1(), featureCall.getFeature());
-
-		if (featureCall.isExplicitOperationCall()) {
-			acceptor.accept(memberFeatureCallElements.getExplicitOperationCallLeftParenthesisKeyword_1_1_2_0_0());
-		}
-		List<XExpression> arguments = featureCall.getMemberCallArguments();
-		if (!arguments.isEmpty()) {
-				acceptor.accept(memberFeatureCallElements.getMemberCallArgumentsXExpressionParserRuleCall_1_1_2_1_0_0(), arguments.get(0), 0);
-				for (int i = 1; i < arguments.size(); i++) {
-					acceptor.accept(memberFeatureCallElements.getMemberCallArgumentsXExpressionParserRuleCall_1_1_2_1_1_1_0(), arguments.get(i), i);
-				}
-		}
-		acceptor.finish();
-	}
+//	@Inject
+//	private JavammGrammarAccess access;
+//
+//	@Override
+//	protected void sequence_XMemberFeatureCall(EObject context, JavammXMemberFeatureCall featureCall) {
+//		INodesForEObjectProvider nodes = createNodeProvider(featureCall);
+//		SequenceFeeder acceptor = createSequencerFeeder(featureCall, nodes);
+//		XMemberFeatureCallElements memberFeatureCallElements= access.getXMemberFeatureCallAccess();
+//
+//		// we manually modify the structure of this element, so we must serialize that accordingly
+//		acceptor.accept(memberFeatureCallElements.getJavammXMemberFeatureCallMemberCallTargetAction_1_1_0_0_0(),
+//				featureCall.getArrayAccessExpression().getArray());
+//
+//		final List<XExpression> indexes = featureCall.getIndexes();
+//		acceptor.accept(memberFeatureCallElements.getIndexesXExpressionParserRuleCall_1_1_0_0_2_0(),
+//				indexes.get(0), 0);
+//		for (int i = 1; i < indexes.size(); i++) {
+//			acceptor.accept(memberFeatureCallElements.getIndexesXExpressionParserRuleCall_1_1_0_0_4_1_0(),
+//					indexes.get(i), i);
+//		}
+//
+//		acceptor.accept(memberFeatureCallElements.getFeatureJvmIdentifiableElementIdOrSuperParserRuleCall_1_1_1_0_1(), featureCall.getFeature());
+//
+//		if (featureCall.isExplicitOperationCall()) {
+//			acceptor.accept(memberFeatureCallElements.getExplicitOperationCallLeftParenthesisKeyword_1_1_2_0_0());
+//		}
+//		List<XExpression> arguments = featureCall.getMemberCallArguments();
+//		if (!arguments.isEmpty()) {
+//				acceptor.accept(memberFeatureCallElements.getMemberCallArgumentsXExpressionParserRuleCall_1_1_2_1_0_0(), arguments.get(0), 0);
+//				for (int i = 1; i < arguments.size(); i++) {
+//					acceptor.accept(memberFeatureCallElements.getMemberCallArgumentsXExpressionParserRuleCall_1_1_2_1_1_1_0(), arguments.get(i), i);
+//				}
+//		}
+//		acceptor.finish();
+//	}
 
 }
