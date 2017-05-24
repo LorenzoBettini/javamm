f808a91bf2d0dc31edafa09504281852618553e6
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2017-05-24 15:01:47 +0200

    disable p2 mirrors in travis build

--- travis_build.sh
+++ travis_build.sh
@@ -4,12 +4,12 @@ set -ev
 if [ "$TRAVIS_OS_NAME" == "osx" ]; then
 	if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
 		echo "Build on MacOSX: Pull Request"
-		mvn -f javamm.releng/pom.xml clean verify -U -Dfindbugs.skip=true -Pide-tests
+		mvn -f javamm.releng/pom.xml clean verify -U -Dfindbugs.skip=true -Pide-tests -Dtycho.disableP2Mirrors=true
 	else
 		echo "Build on MacOSX for standard commit"
-		mvn -f javamm.releng/pom.xml clean verify -U -Dfindbugs.skip=true -Pide-tests
+		mvn -f javamm.releng/pom.xml clean verify -U -Dfindbugs.skip=true -Pide-tests -Dtycho.disableP2Mirrors=true
 	fi
 else
 	echo "Build on Linux"
-	mvn -f javamm.releng/pom.xml clean verify -U -Dfindbugs.skip=true -Pide-tests -Pjacoco-report coveralls:report
+	mvn -f javamm.releng/pom.xml clean verify -U -Dfindbugs.skip=true -Pide-tests -Pjacoco-report coveralls:report -Dtycho.disableP2Mirrors=true
 fi 
