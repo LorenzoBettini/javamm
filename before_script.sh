18d93b00c42022e2b8ad0253bda3742fcd743eea
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2018-01-04 10:23:04 +0100

    use mutter instead of metacity in travis

--- before_script.sh
+++ before_script.sh
@@ -4,5 +4,5 @@ set -ev
 if [ "${TRAVIS_OS_NAME}" = "linux" ]; then
 	sh -e /etc/init.d/xvfb start
 	sleep 5
-	metacity --sm-disable --replace 2> metacity.err &
+	mutter --sm-disable --replace 2> mutter.err &
 fi 
