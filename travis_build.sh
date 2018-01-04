aa41659d603ba4d3d9abf05f0f021189f8e5b6b7
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2018-01-04 09:54:57 +0100

    macosx build in travis only on PR

--- travis_build.sh
+++ travis_build.sh
@@ -4,12 +4,11 @@ set -ev
 if [ "$TRAVIS_OS_NAME" == "osx" ]; then
 	if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
 		echo "Build on MacOSX: Pull Request"
-		mvn -f javamm.releng/pom.xml clean verify -U -Dfindbugs.skip=true -Pide-tests -Dtycho.disableP2Mirrors=true
+		mvn -f javamm.releng/pom.xml clean verify -Pide-tests
 	else
-		echo "Build on MacOSX for standard commit"
-		mvn -f javamm.releng/pom.xml clean verify -U -Dfindbugs.skip=true -Pide-tests -Dtycho.disableP2Mirrors=true
+		echo "Skipping build on MacOSX for standard commit"
 	fi
 else
 	echo "Build on Linux"
-	mvn -f javamm.releng/pom.xml clean verify -U -Dfindbugs.skip=true -Pide-tests -Pjacoco-report coveralls:report -Dtycho.disableP2Mirrors=true
+	mvn -f javamm.releng/pom.xml clean verify -Pide-tests $ADDITIONAL
 fi 
