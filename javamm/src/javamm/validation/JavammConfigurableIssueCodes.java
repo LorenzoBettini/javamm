5204f83f14a24e0c892090cd11b97de44703d9b7
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2021-06-25 15:53:33 +0200

    fixed access to IssueCodes.COPY_JAVA_PROBLEMS

--- javamm/src/javamm/validation/JavammConfigurableIssueCodes.java
+++ javamm/src/javamm/validation/JavammConfigurableIssueCodes.java
@@ -3,6 +3,8 @@
  */
 package javamm.validation;
 
+import static org.eclipse.xtext.validation.IssueCodes.COPY_JAVA_PROBLEMS;
+
 import org.eclipse.xtext.preferences.PreferenceKey;
 import org.eclipse.xtext.util.IAcceptor;
 import org.eclipse.xtext.validation.SeverityConverter;
@@ -20,7 +22,7 @@ public class JavammConfigurableIssueCodes extends XbaseConfigurableIssueCodes {
 	protected void initialize(IAcceptor<PreferenceKey> iAcceptor) {
 		super.initialize(iAcceptor);
 		// overwrite xbase default
-		iAcceptor.accept(create(org.eclipse.xtext.xbase.validation.IssueCodes.COPY_JAVA_PROBLEMS,
+		iAcceptor.accept(create(COPY_JAVA_PROBLEMS,
 				SeverityConverter.SEVERITY_ERROR));
 	}
 }
