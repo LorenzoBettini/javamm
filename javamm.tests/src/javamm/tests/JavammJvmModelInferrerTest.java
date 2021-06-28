295e9cdce092361309fdfd23ad66cabfee10e553
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2021-06-28 18:20:16 +0200

    112: model inferrer test in Java
    Task-Url: https://github.com/LorenzoBettini/javamm/issues/112

---
+++ javamm.tests/src/javamm/tests/JavammJvmModelInferrerTest.java
@@ -0,0 +1,28 @@
+package javamm.tests;
+
+import org.eclipse.xtext.testing.InjectWith;
+import org.eclipse.xtext.testing.XtextRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.google.inject.Inject;
+
+import javamm.javamm.JavammFactory;
+import javamm.jvmmodel.JavammJvmModelInferrer;
+
+@RunWith(XtextRunner.class)
+@InjectWith(JavammInjectorProvider.class)
+public class JavammJvmModelInferrerTest {
+	@Inject
+	private JavammJvmModelInferrer inferrer;
+
+	@Test
+	public void testWithANonJavammProgram() {
+		inferrer.infer(JavammFactory.eINSTANCE.createJavammMethod(), null, false);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testWithNull() {
+		inferrer.infer(null, null, false);
+	}
+}
