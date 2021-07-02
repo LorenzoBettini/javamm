094ecbb13fa89f5df343be40a97bddb8bc419e0c
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2021-07-02 13:41:00 +0200

    start from an empty workspace

--- javamm.ui.tests/src/javamm/ui/tests/CustomAbstractWorkbenchTest.java
+++ javamm.ui.tests/src/javamm/ui/tests/CustomAbstractWorkbenchTest.java
@@ -1,6 +1,11 @@
 package javamm.ui.tests;
 
+import static org.eclipse.xtext.ui.testing.util.IResourcesSetupUtil.cleanWorkspace;
+import static org.eclipse.xtext.ui.testing.util.IResourcesSetupUtil.waitForBuild;
+
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.xtext.ui.testing.AbstractWorkbenchTest;
+import org.junit.BeforeClass;
 
 /**
  * Custom version that avoids removing projects from the workspace.
@@ -10,9 +15,13 @@ import org.eclipse.xtext.ui.testing.AbstractWorkbenchTest;
  */
 public abstract class CustomAbstractWorkbenchTest extends AbstractWorkbenchTest {
 
-	/**
-	 * Avoids deleting project
-	 */
+	@BeforeClass
+	public static void clean() throws CoreException {
+		closeEditors();
+		cleanWorkspace();
+		waitForBuild();
+	}
+
 	@Override
 	public void setUp() throws Exception {
 		closeWelcomePage();
@@ -28,4 +37,5 @@ public abstract class CustomAbstractWorkbenchTest extends AbstractWorkbenchTest 
 		closeEditors();
 		waitForEventProcessing();
 	}
+
 }
